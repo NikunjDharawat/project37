2478209f02c6747b9cbf57bd611ac444863e5bb9
NikunjDharawat <74286044+NikunjDharawat@users.noreply.github.com>
2021-02-20 18:43:22 +0530

    Add files via upload

---
+++ Question.js
@@ -0,0 +1,33 @@
+class Question{
+    constructor() {
+      this.input= createInput("Name");
+      this.button= createButton('Play');
+      this.greeting = createElement('h2');
+    }
+  hide(){
+    this.greeting.hide();
+    this.button.hide();
+    this.input.hide();
+  
+  }
+    display(){
+        var title = createElement('h2')
+            this.title.html("MyQuiz Game");
+            this.title.position(350,0);
+
+            this.question.html("Question:- what starts and  ends with the letters 'E',but has only one letters? " );
+            this.question.position(150,80);
+            this.option1.html("1: Everyone ");
+            this.option1.position(150,100);
+            this.option2.html("2: Envelope");
+            this.option2.position(150,120);
+
+            this.input1.position(150,230);
+    
+        
+        }
+      
+      
+     
+    }
+  
